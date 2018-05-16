eeaea37387597e2fa57e9a4f6ec6676aa0cfbbd9
WesoleKalorie <38699814+WesoleKalorie@users.noreply.github.com>
2018-05-16 16:45:25 +0200

    Add files via upload

---
+++ HappyCalories/HappyCalories/LoginViewPresenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyCalories
+{
+    class LoginViewPresenter
+    {
+        IView view;
+        Model model;
+
+        public LoginViewPresenter(Model model, IView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+    }
+}
---
+++ HappyCalories/HappyCalories/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HappyCalories
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            LoginView loginView = new LoginView();
+            Model model = new Model();
+            LoginViewPresenter loginViewPresenter = new LoginViewPresenter(model, loginView);
+            Application.Run(loginView);
+        }
+    }
+}
